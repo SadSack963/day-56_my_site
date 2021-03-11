72d6543b1fbff77e38f1782ee19dcf1477f208fd
john <john@jpatmore.plus.com>
2021-03-11 19:15:12 +0000

    CV working with template and static files

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# url_for('static', filename='/css/styles.css')
+# url_for('static', filename='/images/fun ride.css')
 
 @app.route('/')
 def home_page(name=None):
-    return render_template('index.html', name=name)
+    return render_template('./my_site/index.html', name=name)
 
 
 if __name__ == '__main__':
