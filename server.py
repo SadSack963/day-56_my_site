f05da298514b881351050a45d02b0afb8660927b
john <john@jpatmore.plus.com>
2021-03-11 21:09:13 +0000

    Challenge 1 completed

--- server.py
+++ server.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# url_for('static', filename='/css/styles.css')
-# url_for('static', filename='/images/fun ride.css')
 
 @app.route('/')
-def home_page(name=None):
-    return render_template('./my_site/index.html', name=name)
+def home_page():
+    return render_template('./my_site/challenge.html')
 
 
 if __name__ == '__main__':
