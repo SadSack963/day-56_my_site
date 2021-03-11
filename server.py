2aa7ea3d8ab955b60d10b589f7c979bfce05c28c
john <john@jpatmore.plus.com>
2021-03-11 21:46:51 +0000

    Challenge 2 completed

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('./my_site/challenge.html')
+    # return render_template('./my_site/index.html')
+    # return render_template('./my_site/challenge.html')
+    return render_template('./challenge2/index.html')
+
 
 
 if __name__ == '__main__':
